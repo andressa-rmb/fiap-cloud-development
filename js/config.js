030e5b7bab0f49a9a19f08b4603c047e02912551
andressa-rmb <andressa.rmb@gmail.com>
2019-06-15 13:38:25 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9IWuqLvL8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ip7gt0bahjunkdjnm67gv7m9i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
